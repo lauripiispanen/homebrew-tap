48d7626093df014dc11c56f85adaf54371998421
Lauri Piispanen <lauripiispanen@users.noreply.github.com>
2026-03-09 13:50:59 +0200

    Update git-workset to v0.1.2

--- Formula/git-workset.rb
+++ Formula/git-workset.rb
@@ -1,30 +1,30 @@
 class GitWorkset < Formula
   desc "Named sparse-checkout profiles for git worktrees"
   homepage "https://github.com/lauripiispanen/git-workset"
-  version "0.1.1"
+  version "0.1.2"
   license "MIT"
 
   on_macos do
     on_intel do
       url "https://github.com/lauripiispanen/git-workset/releases/download/v#{version}/git-workset-x86_64-apple-darwin.tar.gz"
-      sha256 "742ba9b5f299b511315355269a3a1c6fa262752955eb8464c9655ad870c9319d"
+      sha256 "b721437444be8c9d0730b22fd03085c0912e251442012c062d314c7e143bbfee"
     end
 
     on_arm do
       url "https://github.com/lauripiispanen/git-workset/releases/download/v#{version}/git-workset-aarch64-apple-darwin.tar.gz"
-      sha256 "6c11be53e4be67e95465c3c7fd0447f15adcb40aa91293d3d69e2aac8749a815"
+      sha256 "86c9e8b7c568e669657176d9a7ae2700ec293fa44584f0b3fd414f326975de4d"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/lauripiispanen/git-workset/releases/download/v#{version}/git-workset-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "484480f9b7be8c7dcc6f88b0ce84c4645831ec4c88a803f6d1b3a0d0d48d4410"
+      sha256 "3c4e28aed50a17d1e0b223d84c66ef57d77131e39130c2b4aa01fb4ae187c322"
     end
 
     on_arm do
       url "https://github.com/lauripiispanen/git-workset/releases/download/v#{version}/git-workset-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "5d10a4485ca49c4998059e624c3bfe561bb5ba170274911c55460e642253a5ad"
+      sha256 "5d85b123a529c9b99ba8b0742d9c9129f6182f9a49b28897a128584f24279b89"
     end
   end
 
